9e229fb30ae30c5fcc31185ddef37c7c0be3858b
Paul Curry <pcurry@gmail.com>
2023-12-10 00:30:21 -0500

    Broke out logic, added testing, still gets 53998

--- day-01/problem_two.py
+++ day-01/problem_two.py
@@ -26,7 +26,7 @@ DIGITS_LOOKUP = {
 }
 
 
-def process_line(line):
+def tokenize_line(line):
     unprocessed = line
     processed = []
     while unprocessed:
@@ -36,17 +36,72 @@ def process_line(line):
             print("Matched")
             print(match.groups())
 
-            processed.append(DIGITS_LOOKUP[match.group(1)])
+            processed.append(match.group(1))
             unprocessed = match.group(2)
         else:
             print("Not Matched")
             unprocessed = unprocessed[1:]
 
     print(processed)
-    first_digit = processed[0]
-    last_digit = processed[-1]
-    result = int(first_digit + last_digit)
-    return result
+    return processed
+
+
+def extract_first_last(tokens):
+    first = DIGITS_LOOKUP[tokens[0]]
+    last = DIGITS_LOOKUP[tokens[-1]]
+    return int(first + last)
+
+
+def test_extract():
+    test1 = ["1", "4"]
+    assert extract_first_last(test1) == 14
+
+    test2 = ["5"]
+    assert extract_first_last(test2) == 55
+
+    test3 = ["seven", "4"]
+    assert extract_first_last(test3) == 74
+    
+
+def test_tokenize():
+    test1 = tokenize_line("two1nine")
+    assert len(test1) == 3
+    assert test1[0] == "two"
+    assert test1[-1] == "nine"
+    assert extract_first_last(test1) == 29
+
+    test2 = tokenize_line("eightwothree")
+    assert len(test2) == 2
+    assert test2[0] == "eight"
+    assert test2[-1] == "three"
+    assert extract_first_last(test2) == 83
+
+    test3 = tokenize_line("abcone2threexyz")
+    assert len(test3) == 3
+    assert test3[0] == "one"
+    assert test3[-1] == "three"
+    assert extract_first_last(test3) == 13
+
+    test4 = tokenize_line("xtwone3four")
+    assert len(test4) == 3
+    assert test4[0] == "two"
+    assert test4[-1] == "four"
+    
+
+    test5 = tokenize_line("4nineeightseven2")
+    assert len(test5) == 5
+    assert test5[0] == "4"
+    assert test5[-1] == "2"
+
+    test6 = tokenize_line("zoneight234")
+    assert len(test6) == 4
+    assert test6[0] == "one"
+    assert test6[-1] == "4"
+
+    test7 = tokenize_line("7pqrstsixteen")
+    assert len(test7) == 2
+    assert test7[0] == "7"
+    assert test7[-1] == "six"
 
 
 if __name__ == "__main__":
@@ -56,9 +111,14 @@ if __name__ == "__main__":
     with open(calib_file, "r") as fin:
         calib_lines = fin.readlines()
 
+    test_extract()
+    test_tokenize()
+
     # process_line(calib_lines[0])
-    processed_lines = [
-        process_line(line) for line in calib_lines
+    line_values = [
+        extract_first_last(tokenize_line(line)) for line in calib_lines
     ]
-    print(len(processed_lines))
-    print(sum(processed_lines))
+    print(sum(line_values))
+    # print(len(processed_lines))
+    # print(sum(processed_lines))
+    
